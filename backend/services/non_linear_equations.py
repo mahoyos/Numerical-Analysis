bcffad90ae02e1d52d13848fce7264cf71b2795f
JuanM0412 <juangomezp1204@gmail.com>
2024-10-07 15:51:59 -0500

    feat: Newton-Raphson method for non-linear equations

--- backend/services/non_linear_equations.py
+++ backend/services/non_linear_equations.py
@@ -31,3 +31,34 @@ class NonLinearEquationsService:
             "iterations": iteration_data
         }
         return result
+
+    @staticmethod
+    def newton_raphson_service(
+        initial_guess: float,
+        tolerance: float,
+        max_iterations: int,
+        function_expression: str
+    ) -> Dict[str, Any]:
+        iteration_data = []
+        x = initial_guess
+        derivative_expression = MathOperations.get_derivative(function_expression)
+        f_value = MathOperations.evaluate_function(function_expression, x)
+        f_derivative_value = MathOperations.evaluate_function(derivative_expression, x)
+        error = 100
+        iteration_count = 0
+        iteration_data.append((iteration_count, x, f_value, error))
+
+        while error > tolerance and f_value != 0 and f_derivative_value != 0 and iteration_count < max_iterations:
+            x_new = x - f_value / f_derivative_value
+            f_value = MathOperations.evaluate_function(function_expression, x_new)
+            f_derivative_value = MathOperations.evaluate_function(derivative_expression, x_new)
+            iteration_count += 1
+            error = abs(x_new - x)
+            x = x_new
+            iteration_data.append((iteration_count, x, f_value, error))
+
+        result = {
+            "root": x,
+            "iterations": iteration_data
+        }
+        return result
